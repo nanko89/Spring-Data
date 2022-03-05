46ed21245ba6f1b7f4b68734fdcf7bebdf3752c8
nanko89 <natali.v.angelova@gmail.com>
2022-03-05 16:55:07 +0200

    SpringDataIntroLab

---
+++ SpringDataIntroLab/src/main/java/com/example/springdataintrolab/services/UserService.java
@@ -0,0 +1,8 @@
+package com.example.springdataintrolab.services;
+
+import com.example.springdataintrolab.moduls.Account;
+import com.example.springdataintrolab.moduls.User;
+
+public interface UserService {
+    void registerUser(User user);
+}
