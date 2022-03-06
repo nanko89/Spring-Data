0296ef8a060b25f210a2349a00cbd33c20213db0
nanko89 <natali.v.angelova@gmail.com>
2022-03-07 00:07:39 +0200

    Spring Data Intro Exercise

---
+++ SpringDataIntroExercise/src/test/java/bookshop/system/SpringDataIntroExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package bookshop.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataIntroExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
