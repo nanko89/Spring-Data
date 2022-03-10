e0f43d8dccc9d7a7eb5eeefe36f05a860951ad8e
nanko89 <natali.v.angelova@gmail.com>
2022-03-10 18:27:32 +0200

    Spring Data Auto Mapping Object

---
+++ SpringDataAutoMappingObjectsLab/src/main/java/com/example/springdataautomappingobjectslab/SpringDataAutoMappingObjectsLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataautomappingobjectslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAutoMappingObjectsLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataAutoMappingObjectsLabApplication.class, args);
+    }
+
+}
---
+++ SpringDataAutoMappingObjectsLab/src/test/java/com/example/springdataautomappingobjectslab/SpringDataAutoMappingObjectsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdataautomappingobjectslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAutoMappingObjectsLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
