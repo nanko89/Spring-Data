bb2c3db377b67b61bd991c555c09b96a9d3e35cb
nanko89 <natali.v.angelova@gmail.com>
2022-03-12 19:31:27 +0200

    Spring Data Auto Mapping Object Exercise

---
+++ SpringDataAutoMappingObject Exercise/src/main/java/auto/mapping/exercise/AutoMappingApplication.java
@@ -0,0 +1,13 @@
+package auto.mapping.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMappingApplication.class, args);
+    }
+
+}
---
+++ SpringDataAutoMappingObject Exercise/src/test/java/auto/mapping/exercise/AutoMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package auto.mapping.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
