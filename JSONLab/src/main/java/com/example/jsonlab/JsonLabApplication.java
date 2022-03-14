157f7f795751879eff5a2e2f30397926635ec259
nanko89 <natali.v.angelova@gmail.com>
2022-03-14 22:10:15 +0200

    JSON Lab

---
+++ JSONLab/src/main/java/com/example/jsonlab/JsonLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonLabApplication.class, args);
+    }
+
+}
---
+++ JSONLab/src/test/java/com/example/jsonlab/JsonLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
