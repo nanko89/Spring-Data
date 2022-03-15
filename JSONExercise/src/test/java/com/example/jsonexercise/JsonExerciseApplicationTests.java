1404ed7d9f68629bdce33671e37ab8140422e567
nanko89 <natali.v.angelova@gmail.com>
2022-03-15 16:16:49 +0200

    JSON Processing Exercise

---
+++ JSONExercise/src/test/java/com/example/jsonexercise/JsonExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
