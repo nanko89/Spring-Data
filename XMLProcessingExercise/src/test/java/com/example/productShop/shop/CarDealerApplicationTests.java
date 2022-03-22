fb2e6a43f68b09d1b81232a05d93003a94a431d5
nanko89 <natali.v.angelova@gmail.com>
2022-03-22 17:14:48 +0200

    XML processing Exercise

---
+++ XMLProcessingExercise/src/test/java/com/example/productShop/shop/CarDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productShop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
