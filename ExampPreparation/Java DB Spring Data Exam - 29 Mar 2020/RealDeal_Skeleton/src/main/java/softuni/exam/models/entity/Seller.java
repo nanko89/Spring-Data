f4082c6959d784e14fd626131624e9ad54539bbc
nanko89 <natali.v.angelova@gmail.com>
2022-03-24 18:01:52 +0200

    Java DB Spring Data Exam - 29 Mar 2020

---
+++ ExampPreparation/Java DB Spring Data Exam - 29 Mar 2020/RealDeal_Skeleton/src/main/java/softuni/exam/models/entity/Seller.java
@@ -0,0 +1,82 @@
+package softuni.exam.models.entity;
+
+import softuni.exam.models.entity.enums.Rating;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", length = 20)
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+//    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Rating rating;
+
+    @Column(nullable = false)
+    private String town;
+
+    public Seller() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
