d8a4ecec58b9b78641eebad8353bb2a4a7468021
nanko89 <natali.v.angelova@gmail.com>
2022-03-25 13:27:51 +0200

    Java DB Spring Data Retake Exam - 13 August 2021

---
+++ ExampPreparation/Java DB Spring Data Exam - 13 August 2021/src/main/java/exam/model/entity/Town.java
@@ -0,0 +1,54 @@
+package exam.model.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "towns")
+public class Town {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    @Column
+    private Integer population;
+    @Column(columnDefinition = "TEXT")
+    private String travelGuide;
+
+    public Town() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public String getTravelGuide() {
+        return travelGuide;
+    }
+
+    public void setTravelGuide(String travelGuide) {
+        this.travelGuide = travelGuide;
+    }
+}
