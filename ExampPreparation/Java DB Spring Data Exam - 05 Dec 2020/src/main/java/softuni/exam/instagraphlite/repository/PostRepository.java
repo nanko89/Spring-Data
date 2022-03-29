235d03d3f8267ae52a4564531ab03c2d3d1c10bf
Натали Ангелова <86059352+nanko89@users.noreply.github.com>
2022-03-29 12:58:45 +0300

    Delete ExampPreparation/Java DB Spring Data Exam - 05 Dec 2020 directory

--- ExampPreparation/Java DB Spring Data Exam - 05 Dec 2020/src/main/java/softuni/exam/instagraphlite/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package softuni.exam.instagraphlite.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import softuni.exam.instagraphlite.models.entity.Post;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
