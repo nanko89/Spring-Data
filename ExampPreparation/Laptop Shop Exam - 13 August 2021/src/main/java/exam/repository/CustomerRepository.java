2426657323f37276c123d3a0abcb3070f16a0d65
Натали Ангелова <86059352+nanko89@users.noreply.github.com>
2022-04-01 15:28:46 +0300

    Delete ExampPreparation/Laptop Shop Exam - 13 August 2021 directory

--- ExampPreparation/Laptop Shop Exam - 13 August 2021/src/main/java/exam/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package exam.repository;
-
-import exam.model.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
