8e43d02c2b2cb9ae4dded01fd25d29dda90609f5
nanko89 <natali.v.angelova@gmail.com>
2022-05-05 23:30:36 +0300

    Workshop-MVC Project

---
+++ Workshop-MVC Project/src/main/java/com/example/nlt/NltApplication.java
@@ -0,0 +1,13 @@
+package com.example.nlt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NltApplication.class, args);
+    }
+
+}
---
+++ Workshop-MVC Project/src/test/java/com/example/nlt/NltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
